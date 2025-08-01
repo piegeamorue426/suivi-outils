d56cb83ee823b97fec6151d220b47bc8e7468643
istmyloveee <135707065+piegeamorue426@users.noreply.github.com>
2025-08-01 16:35:55 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+const outils = [
+    { nom: "Clé plate", code: "CL01" },
+    { nom: "Perceuse", code: "PC01" },
+    { nom: "Thermo rétractable", code: "TH01" },
+    { nom: "Visseuse", code: "VS01" },
+    { nom: "Pompe à pression", code: "PP01" },
+    { nom: "Glue", code: "GL01" },
+    { nom: "Foret", code: "FR01" }
+];
