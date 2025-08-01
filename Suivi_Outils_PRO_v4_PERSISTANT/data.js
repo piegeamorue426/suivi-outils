7bcb208813d8b1e5a5cb1e0c58c111fcbbf7457e
istmyloveee <135707065+piegeamorue426@users.noreply.github.com>
2025-08-01 16:36:14 +0200

    Delete Suivi_Outils_PRO_v4_PERSISTANT directory

--- Suivi_Outils_PRO_v4_PERSISTANT/data.js
+++
@@ -1,9 +0,0 @@
-const outils = [
-    { nom: "Clé plate", code: "CL01" },
-    { nom: "Perceuse", code: "PC01" },
-    { nom: "Thermo rétractable", code: "TH01" },
-    { nom: "Visseuse", code: "VS01" },
-    { nom: "Pompe à pression", code: "PP01" },
-    { nom: "Glue", code: "GL01" },
-    { nom: "Foret", code: "FR01" }
-];
